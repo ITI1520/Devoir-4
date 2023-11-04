18b47fc40b347128f35dd380d7187aa8c6e01f0c
Nicola Baker <145513846+NicolaBaker@users.noreply.github.com>
2023-11-04 14:12:56 +0000

    Q1

--- d4.py
+++ d4.py
@@ -1,3 +1,22 @@
 # Devoir 4
-print("Hello")
-print("Hello")
+
+def transl(d, s):
+    
+    if s in d:
+        af = d[s]
+
+    elif s in d.values():
+        for englais, value in d.items():
+            if value == s:
+                af = englais
+    else:
+        af = "Unknown"
+    return af
+
+d = {"apple": "pomme", "banana": "banane", "pear": "poire", "plum": "prune"}
+# Exemples
+print(transl(d, "apple"))  
+print(transl(d, "banane"))  
+print(transl(d, "pear"))  
+print(transl(d, "prune"))  
+print(transl(d, "cy"))  
