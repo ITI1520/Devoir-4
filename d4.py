abdfa3077e8da1ed5f199dc307355e0b1adb9d70
SamuelDicaire <149833122+SamuelDicaire@users.noreply.github.com>
2023-11-09 00:23:31 +0000

    Fini tout les commentaire pour d4.py

--- d4.py
+++ d4.py
@@ -1,57 +1,62 @@
 # Devoir 4
 # Q1
-def transl(d, s):
-    """()"""
-    af = "Unknown"
-    if s in d:
-        af = d[s]
-    elif s in d.values():
-        for englais, value in d.items():
-            if value == s:
-                af = englais
-    return af
+def transl(d, s):                        # Fonction trans1 qui prend en entrez d(dictionaire), s(string)
+    """ (dict{str}, str) -> str """      # Contrat de type
+    af = "Unknown"                       # Initialise af a "Unknown"
+
+    if s in d:                           # Vérifie si "s" est une clée dans le dictionaire "d"         
+        af = d[s]                        # Si oui on affiche la traduction qui est dans le dictionaire "d"
+    elif s in d.values():                #
+        for anglais, value in d.items(): #
+            if value == s:               # Si value est égal a s
+                af = anglais             # On initialise af a "anglais"
+    return af                            # Retourne la valeur de af
 # Q2
-def setOp(list1, list2):
-    """()"""
-    list = set(list1 + list2)
-    return list
+def setOp(list1, list2):                                   # Fonction setOp qui prend en entrez list1(list), list2(list)
+    """ List[[float], [float]] -> dict{float}  """         # Contrat de type
+    list = set(list1 + list2)                              # Initialise list a "set(list1 + list2)" et le tranforme en dictionaire
+    return list                                            # Retourne list en dictionaire
 # Q3
-def matrixMinMax(m):
-    """() pas sur"""
-    maxx = max(m[0])
-    minn = min(m[0]) # max dans la première rangée
- #utilise la fonction Python max 
-    rang = 1 # verification de la rangée 1
+def matrixMinMax(m):                                       # Fonction matrixMinMax qui prend en entrez une liste avec 2 liste a l'interieure                        
+    """ List[[float],[float]] -> Tuple(float, float) """   # Contrat de type
+    maxx, minn = max(m[0]), min(m[0])                      # On initialise maxx a "max(m[0])" et minn a "min(m[0])"
+    
  
-    while rang < len(m):
-        rangMax = max(m[rang])
-        rangMin = min(m[rang]) 
-        if rangMax > maxx: 
-            maxx = rangMax
-        if rangMin < minn: 
-            minn = rangMin
-        rang = rang + 1 
-    return (minn, maxx)
+    rang = 1                                            # On initialise rang avec une valeur de 1
+ 
+    while rang < len(m):                                # Boucle while qui s'exécute tant que rang est plus petit que la longeur de m 
+        rangMax, rangMin = max(m[rang]), min(m[rang])   # Initialise ranMax a "max(m[rang])" et rangMin a "max(m[rang])"
+
+        if rangMax > maxx:                              # Si rangMax est supérieur a maxx
+            maxx = rangMax                              # maxx devient rangMax
+        if rangMin < minn:                              # Si rangMin est inferieur a minn
+            minn = rangMin                              # minn devient rangMin
+        rang = rang + 1                                 # On fait l'incrementation de la valeur de rang
+
+    return (minn, maxx)                                 # On retourne (minn, maxx) en tuples
 
 
-# Exemples
-print("Question 1")
-d = {"apple": "pomme", "banana": "banane", "pear": "poire", "plum": "prune"}
+# Exemple question 1
+print("Question 1")                                                          # Imprimer le message pour la question 1
+d = {"apple": "pomme", "banana": "banane", "pear": "poire", "plum": "prune"} # Le dictionaire "d" pour la question 1
+# Imprimer les 4 exemples de la question 1
 print(transl(d,"pear"))
 print(transl(d,"poire"))  
 print(transl(d,"appricot"))  
 print()
 
-# Exemples 2
-print("Question 2")
+# Exemples question 2
+print("Question 2")                 # Imprimer le message pour la question 2
+# Imprimer les 4 exemples de la question 2
 print(setOp([1,2,2,3],[2,-6,8,7]))
 print(setOp([],[1,4,2,4,6]))
 print(setOp([1,1,1,1],[]))
 print(setOp([1],[2,3,2,2]))
-print()
+print()                             
 
-# Exemple 3
-print("Question 3")
+# Exemple de la question 3
+print("Question 3")                       # Imprimer le message pour la question 3
+# Imprimer les 4 exemple de la question 3
 print(matrixMinMax([[1,5],[2,8]])) 
 print(matrixMinMax([[1,5,10],[2,8,-1]]))
 print(matrixMinMax([[2,8,-1]]))
